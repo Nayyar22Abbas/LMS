18c0f6d081e1f165863d3e254ce8eb4e99392130
unknown <ahsanaliziddi@gmail.com>
2024-06-21 01:10:02 +0500

    loginPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Component/Frontend/Loginpage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { LoginPage } from './Component/Frontend/Loginpage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    <Router>
+      {/* <Route  path="/" Component={Loginpage} /> */}
+      <Routes>
+        {/* <Route  path="/dashboard" Component={Loginpage} /> */}
+        <Route path="" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  
+  )
 }
 
 export default App;
---
+++ src/Component/Frontend/Loginpage.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import './LoginPage.css';
+import { useNavigate } from 'react-router-dom';
+
+
+export const LoginPage: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // Simulating an API call to validate credentials
+    if (username === "Ahsan ali" && password === "1234") {
+      setLoggedIn(true);
+      navigate('/dashboard');
+    } else {
+      alert('Incorrect username or password');
+    }
+  };
+
+  const handleAdminLogin = () => {
+    // Simulating an API call to validate credentials
+    if (username === "admin" && password === "1234") {
+      setLoggedIn(true);
+      navigate('/dashboard/AdminDashboard');
+    } else {
+      alert('Incorrect admin username or password');
+    }
+  };
+
+  return (
+    <div
+      className="login-container"
+      style={{
+        backgroundImage: 'url("lib.jpg")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh'
+      }}
+    >
+      <div className="login-form" >
+        <img src="lib2.jpg" style={{marginLeft:'30%' }} alt="Air University Logo" width={150} height={120} />
+        <h2>Welcome to LMS!</h2>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="login-form-input"
+          />
+        </div>
+        <div>
+          <label>Reg ID:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="login-form-input"
+          />
+        </div>
+        <button onClick={handleLogin} className="login-form-button" style={{marginBottom:'10px'}}>
+          Login
+        </button>
+        {loggedIn && <p className="login-form-message">Welcome, {username}!</p>}
+        <button onClick={handleAdminLogin} className="login-form-button">
+          SignUp
+        </button>
+      </div>
+    </div>
+  );
+};
